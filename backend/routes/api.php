1e6da13251978bd65019f9fb1a236b59c8924643
Stephan Mau <git@subecho.net>
2026-01-19 21:52:47 +0100

    add throttle to limit attempts per user, adding api versioning

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,14 +4,20 @@ use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::prefix('v1')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])
+        ->middleware('throttle:10,1');
 
-Route::middleware('auth:sanctum')->group(function () {
-    /* Auth rules */
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::middleware('throttle:60,1')->group(function () {
+            /* Auth rules */
+            Route::get('/user', [AuthController::class, 'user']);
+            Route::post('/logout', [AuthController::class, 'logout']);
 
-    /* Customer rules */
-    Route::get('/customers/stats', [CustomerController::class, 'stats']);
-    Route::apiResource('customers', CustomerController::class);
+            /* Customer rules */
+            Route::get('/customers/stats', [CustomerController::class, 'stats']);
+            Route::apiResource('customers', CustomerController::class);
+        });
+    });
 });
+
