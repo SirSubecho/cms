53ce2897ea8310dd655bd606d6cd8db197beffd3
Stephan Mau <git@subecho.net>
2026-01-19 20:30:49 +0100

    change design error page

--- frontend/src/pages/ErrorNotFound.vue
+++ frontend/src/pages/ErrorNotFound.vue
@@ -1,23 +1,34 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen text-black text-center q-pa-md flex flex-center">
     <div>
-      <div>404 - Seite nicht gefunden</div>
-
-      <div>Die Seite die Sie aufrufen wollen, konnte nicht gefunden werden.</div>
+      <div class="flex flex-center column q-gutter-md">
+        <div class="error-style">404</div>
+        <h1 class="q-my-none">Seite nicht gefunden</h1>
+        <div class="text-h6">Die Seite, die Sie aufrufen möchten, konnte nicht gefunden werden.</div>
+      </div>
 
       <q-btn
         class="q-mt-xl"
-        color="white"
-        text-color="blue"
+        color="black"
+        text-color="white"
         unelevated
         to="/"
-        label="Dashboard"
+        label="Zum Dashboard"
         no-caps
+        icon-right="arrow_forward"
       />
     </div>
   </div>
+
 </template>
 
 <script setup>
 //
 </script>
+
+<style scoped>
+.error-style{
+  font-size: 10rem;
+  line-height: 1;
+}
+</style>
